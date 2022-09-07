afc49df688804b659a5404f54ac990739bdc666e
ntijoh-isac-ekeroth <isac.ekeroth@elev.ga.ntig.se>
2022-09-07 10:48:20 +0200

    ej

--- secrets/lib/secrets.ex
+++ secrets/lib/secrets.ex
@@ -1,35 +1,8 @@
-defmodule Secrets do
-  def secret_add(secret) do
-    fn(value) -> value + secret end
-  end
-
-<<<<<<< HEAD
-  def secret_subtract(secret) do
-    # Please implement the secret_subtract/1 function
-    fn sub -> sub - secret end
-  end
-=======
-//removed unnecessary funcion due to bad code
-//lol
-//who even wrote that?
->>>>>>> 756eb12cafe533dc7ac3f7af699929b3b7a7d269
-
-
-
-
-  def secret_divide(secret) do
-    fn(value) -> div(value,secret) end
-  end
-
-  def secret_and(secret) do
-    fn(value) -> Bitwise.band(value,secret) end
-  end
-
-  def secret_xor(secret) do
-    fn(value) -> Bitwise.bxor(value,secret)end
-  end
-
-  def secret_combine(secret_function1, secret_function2) do
-    fn(value) -> secret_function2.(secret_function1.(value)) end
-  end
-end
+git stash
+Save modified and staged changes
+git stash list
+list stack-order of stashed file changes
+git stash pop
+write working from top of stash stack
+git stash drop
+discard the changes from top of stash stack
